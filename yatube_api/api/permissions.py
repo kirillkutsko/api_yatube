d7e54511ea30d6605cb9fba99764c4844a863ce8
Кирилл Куцко <eads@mail.ru>
2022-07-08 01:32:29 +0300

    _

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -9,6 +9,6 @@ class AuthorOrReadOnly(permissions.BasePermission):
             return True
 
     def has_object_permission(self, request, view, obj):
-        if (request.method in permissions.SAFE_METHODS or
-                obj.author == request.user):
+        if (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user):
             return True
